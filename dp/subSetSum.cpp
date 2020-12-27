3be1f7a8e72ad492cd0d28f323841efef9aeb1e8
techiedheeraj <dheeraj.embdeveloper@gmail.com>
2020-12-27 17:14:58 +0530

    Added two more solutions to subset sum problem

--- dp/subSetSum.cpp
+++ dp/subSetSum.cpp
@@ -15,16 +15,67 @@ class SubsetSum {
 
     SubsetSum(int n, int totals) {
       sum = totals;
-      this->dp.resize(n+1, vector<bool>(w+1, false));
+      this->dp.resize(n+1, vector<bool>(totals+1, false));
     }
 
-    bool subsetSum(vector<int>&weights, int ts) {
-      if ( ts
+    // recursion
+    bool subsetSum1(vector<int>&weights, int n, int ts) {
+      if (ts == 0) return true; //first column
+      if (n == 0) return false;
 
+      if (weights[n-1] <= ts)
+        return subsetSum1(weights, n-1, ts-weights[n-1]) || subsetSum1(weights, n-1, ts);
+      else
+        return subsetSum1(weights, n-1, ts);
     }
 
+    // recursion + memorization
+    bool subsetSum2(vector<int>&weights, int n, int ts) {
+      if (ts == 0) return true; // first column
+      if (n == 0) return false; // first row except first cell
+
+      if (dp[n][ts]) return dp[n][ts]; 
+
+      if (weights[n-1] <= ts)
+        return dp[n][ts] = subsetSum2(weights, n-1, ts-weights[n-1]) || subsetSum2(weights, n-1, ts);
+      else
+        return dp[n][ts] = subsetSum2(weights, n-1, ts);
+    }
+    // iterative method
+    bool subsetSum3(vector<int>&weights, int n, int ts) {
+      for (int i = 0; i < n+1; ++i) { // for size
+        for (int j = 0; j < ts+1; ++j) { // for sum
+          if (j == 0) {
+            dp[i][j] = true;
+            continue;
+          }
+          if (i == 0) {
+            dp[i][j] = false;
+            continue;
+          }
+          if (dp[i][j]) return dp[i][j];
+          if (weights[i-1] <= j)
+            dp[i][j] = dp[i-1][j-weights[i-1]] || dp[i-1][j];
+          else
+            dp[i][j] = dp[i-1][j];
+        }
+      }
+      printDp(dp, n, ts+1);
+      return dp[n][ts];
+    }
     bool subsetSum(vector<int>&weights, int& items) {
-      sumSetSum1(weights, items, this->sum);
+     // return subsetSum1(weights, items, this->sum);
+      //return subsetSum2(weights, items, this->sum);
+      return subsetSum3(weights, items, this->sum);
+    }
+
+    void printDp(vector<vector<bool>>&d, int r, int size) {
+      if (r < 0) return;
+      printDp(d, r-1, size);
+      cout << "| ";
+      for (int i = 0; i < size; ++i)
+        cout << (int)d[r][i] << " ";
+      cout << "|\n";
     }
 };
     
@@ -49,7 +100,9 @@ int main() {
   for (int i =0; i < items; ++i)
     cin >> weights[i];
 
-  cout << "Subset with sum exists: " << boolalpha << ks->subsetSum(weights, items) << endl;
+  auto res = subs->subsetSum(weights, items); 
+
+  cout << "Subset with sum exists: " << boolalpha << res << endl;
 
   return 0;
 }    
