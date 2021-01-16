f6769aab4857a6b9b0f9ee0d46e73391f854573d
techiedheeraj <dheeraj.embdeveloper@gmail.com>
2021-01-16 18:14:41 +0530

    Solved https://leetcode.com/problems/longest-palindromic-subsequence/

--- dp/516_LongestPalindromicSubsequence.cpp
+++ dp/516_LongestPalindromicSubsequence.cpp
@@ -7,6 +7,44 @@
 
 class Solution {
 public:
+    int dp[1001][1001];
+    int lcs(string& s1, string& s2, int n1, int n2) {
+      for (int i= 0; i < n1+1; ++i) {
+        for (int j= 0; j < n2+1; ++j) {
+          if (i == 0 || j == 0) {
+            dp[i][j] = 0; continue;
+          }
+          if (s1[i-1] == s2[j-1])
+            dp[i][j] = 1+dp[i-1][j-1];
+          else
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+      }
+      int len = dp[n1][n2];
+      string res;
+      
+      while(n1 != 0 && n2 != 0) {
+        if (s1[n1-1] == s2[n2-1]) {
+          res.push_back(s1[n1-1]); 
+          --n1; --n2;
+        }
+        else {
+          dp[n1-1][n2] > dp[n1][n2-1] ? --n1 : --n2;
+        }
+      }
+      return len == res.length() ? len : 0;
+    }
+    int longestPalindromeSubseq(string s) {
+      string rev = s;
+      reverse(rev.begin(), rev.end());
+      return lcs(s, rev, s.length(), rev.length());
+    }
+};
+
+
+/*
+class Solution {
+public:
     vector<vector<int>>dp;
     int helper(string&s, string& sb, int l, int m) {
 
@@ -32,3 +70,4 @@ public:
 //      return dp[s.length()][s.length()];
     }
 };
+*/
